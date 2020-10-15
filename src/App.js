bb36edae904a0abe459968a7e54a77c4ec4b3cdd
Ken Vilar <kenvilar@gmail.com>
2020-10-15 09:55:51 +0800

    Make useState a one liner

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 function App() {
   const [query, setQuery] = useState('Sydney, au');
-  const [city, setCity] = useState('');
-  const [temp, setTemp] = useState('');
-  const [condition, setCondition] = useState('');
-  const [country, setCountry] = useState('');
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
 
   const data = async (q) => {
     const apiRes = await fetch(
@@ -21,17 +23,20 @@ function App() {
     e.preventDefault();
 
     data(query).then(res => {
-      setCity(res.name);
-      setTemp(res.main.temp);
-      setCondition(res.weather[0].main);
-      setCountry(res.sys.country);
+      console.log('ken res', res);
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+      });
     });
   };
 
   return (
     <div className="App">
-      <WeatherCard temp={temp} condition={condition} city={city}
-                   country={country}/>
+      <WeatherCard temp={weather.temp} condition={weather.condition}
+                   city={weather.city} country={weather.country}/>
       {/*<WeatherCard temp={20} condition="Dust" city="Melbourne" country="AU"/>*/}
       {/*<WeatherCard temp={40} condition="Tornado" city="London" country="GB"/>*/}
       <form action="">
