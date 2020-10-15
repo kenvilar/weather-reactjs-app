6ae3c9d4aece21c4be5f36ca352ba3aaec1f38b2
Ken Vilar <kenvilar@gmail.com>
2020-10-15 09:20:49 +0800

    Handle API requests

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import WeatherCard from './components/WeatherCard/component';
 import './App.css';
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=`);
+    const resJson = await apiRes.json();
+    return resJson;
+  };
+
+  data().then(res => console.log(res));
+
   return (
     <div className="App">
       <WeatherCard temp={-15} condition="Clear" city="Sydney" country="AU"/>
